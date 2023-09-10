736a6ee3b33b771f8983414f60c9735eae8c963a
Yosh <github@yosh.is>
2023-09-10 02:42:49 +0200

    Add mutation benchmarks

--- benches/differential.rs
+++ benches/differential.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
+use heckcheck::prelude::*;
 
 fn insert(c: &mut Criterion) {
     let mut group = c.benchmark_group("insert");
@@ -27,6 +28,102 @@ fn insert(c: &mut Criterion) {
             b.iter_batched(setup, routine, BatchSize::SmallInput)
         });
     }
+    group.finish();
+}
+
+fn mutate(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mutate");
+    let seed = fastrand::u64(1024..2048);
+
+    /// A single operation we can apply
+    #[derive(Arbitrary, Debug)]
+    enum Operation {
+        Insert(usize),
+        Fetch(usize),
+        Remove(usize),
+        Contains(usize),
+        Clear,
+        Reserve(usize),
+    }
+
+    group.bench_function(BenchmarkId::new("beton", "main"), |b| {
+        let setup = || tree_slab::Slab::new();
+        let routine = |mut slab: tree_slab::Slab<usize>| {
+            black_box({
+                let mut checker = heckcheck::HeckCheck::from_seed(seed);
+                checker.check(|operations: Vec<Operation>| {
+                    for operation in operations {
+                        match operation {
+                            Operation::Insert(value) => {
+                                let _key = slab.insert(value);
+                            }
+                            Operation::Fetch(index) => {
+                                let _output = slab.get(index.into());
+                            }
+                            Operation::Remove(index) => {
+                                let _output = slab.remove(index.into());
+                            }
+                            Operation::Contains(index) => {
+                                let _contains = slab.contains_key(index.into());
+                            }
+                            Operation::Clear => {
+                                slab.clear();
+                            }
+                            Operation::Reserve(capacity) => {
+                                // NOTE: very big allocations make this slow, so we ensure
+                                // they're always a little smaller
+                                //
+                                // FIXME: in the proper impl of BitTree, we should be able
+                                // to a `memset(3)` call.
+                                let capacity = capacity % 1024;
+                                slab.reserve(capacity);
+                            }
+                        }
+                    }
+                    Ok(())
+                });
+            });
+        };
+        b.iter_batched(setup, routine, BatchSize::SmallInput)
+    });
+
+    group.bench_function(BenchmarkId::new("slab", "main"), |b| {
+        let setup = || slab::Slab::new();
+        let routine = |mut slab: slab::Slab<usize>| {
+            black_box({
+                let mut checker = heckcheck::HeckCheck::from_seed(seed);
+                checker.check(|operations: Vec<Operation>| {
+                    for operation in operations {
+                        match operation {
+                            Operation::Insert(value) => {
+                                let _key = slab.insert(value);
+                            }
+                            Operation::Fetch(index) => {
+                                let _output = slab.get(index);
+                            }
+                            Operation::Remove(index) => {
+                                let _output = slab.try_remove(index);
+                            }
+                            Operation::Contains(index) => {
+                                let _contains = slab.contains(index);
+                            }
+                            Operation::Clear => {
+                                slab.clear();
+                            }
+                            Operation::Reserve(capacity) => {
+                                // NOTE: very big allocations make this slow, so we ensure
+                                // they're always a little smaller
+                                let capacity = capacity % 1024;
+                                slab.reserve(capacity);
+                            }
+                        }
+                    }
+                    Ok(())
+                });
+            });
+        };
+        b.iter_batched(setup, routine, BatchSize::SmallInput)
+    });
     group.finish();
 }
 
@@ -80,5 +177,5 @@ fn iterate(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(operations, insert, iterate);
+criterion_group!(operations, insert, mutate, iterate);
 criterion_main!(operations);
